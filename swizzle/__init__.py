7d632013241a15efa1b19ffabd46d3d65271a3c3
Jan <mail@jantmueller.com>
2024-07-08 22:38:47 +0000

    Add name separator as an argument to the decorator

--- swizzle/__init__.py
+++ swizzle/__init__.py
@@ -1,11 +1,16 @@
 from functools import wraps
 import sys
 import types
-import inspect
 
 __version__ = "0.1.1"
 
-def _swizzle(func):
+def _split(s, sep):
+    if sep == '':
+        return list(s)
+    else:
+        return s.split(sep)
+    
+def _swizzle(func, sep = None):
     def _getattr(obj, name, default=object()):
         try:
             return func(obj, name)
@@ -15,29 +20,33 @@ def _swizzle(func):
     @wraps(func)
     def _swizzle_attributes(obj, name):
         """Find attributes of an object that match substrings of a given name."""
-        found_attributes = []
-        sentinel = object()
-        i = 0
-        while i < len(name):
-            match_found = False
-            for j in range(len(name), i, -1):
-                substr = name[i:j]
-                attr = _getattr(obj, substr, sentinel)
-                if attr is not sentinel:
-                    found_attributes.append(attr)
-                    i = j  # Move index to end of the matched substring
-                    match_found = True
-                    break
-            if not match_found:
-                raise AttributeError(f"No matching attribute found for substring: {name[i:]}")
+        if sep is not None:
+            names = _split(name, sep)
+            found_attributes = [func(obj, n) for n in names]
+        else:
+            found_attributes = []
+            sentinel = object()
+            i = 0
+            while i < len(name):
+                match_found = False
+                for j in range(len(name), i, -1):
+                    substr = name[i:j]
+                    attr = _getattr(obj, substr, sentinel)
+                    if attr is not sentinel:
+                        found_attributes.append(attr)
+                        i = j  # Move index to end of the matched substring
+                        match_found = True
+                        break
+                if not match_found:
+                    raise AttributeError(f"No matching attribute found for substring: {name[i:]}")
         return tuple(found_attributes)
     return _swizzle_attributes
     
-def swizzle(cls=None, meta = False):
+def swizzle(cls=None, meta = False, sep = None):
     def decorator(cls):
         # Decorate the class's __getattr__ or __getattribute__
         cls_fn = cls.__getattr__ if hasattr(cls, '__getattr__') else cls.__getattribute__
-        setattr(cls, cls_fn.__name__, _swizzle(cls_fn)) 
+        setattr(cls, cls_fn.__name__, _swizzle(cls_fn, sep)) 
 
         # Handle the meta class
         if meta:
@@ -47,7 +56,7 @@ def swizzle(cls=None, meta = False):
                 meta_cls = SwizzleType
                 cls = meta_cls(cls.__name__, cls.__bases__, dict(cls.__dict__))
             meta_fn = meta_cls.__getattr__ if hasattr(meta_cls, '__getattr__') else meta_cls.__getattribute__
-            setattr(meta_cls, meta_fn.__name__, _swizzle(meta_fn))
+            setattr(meta_cls, meta_fn.__name__, _swizzle(meta_fn, sep))
         return cls
 
     if cls is None:
@@ -63,8 +72,8 @@ class Swizzle(types.ModuleType):
         types.ModuleType.__init__(self, __name__)
         self.__dict__.update(sys.modules[__name__].__dict__)
     
-    def __call__(self, cls=None, meta=False):
-        return swizzle(cls, meta)
+    def __call__(self, cls=None, meta=False, sep = None):
+        return swizzle(cls, meta, sep)
 
 sys.modules[__name__] = Swizzle()
         
