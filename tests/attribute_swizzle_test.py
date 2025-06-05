e874c6c266fa44b5fc03cf23b645218c24f63f57
janthmueller <mail@jantmueller.com>
2025-06-05 21:56:35 +0200

    switched to pytest

--- tests/attribute_swizzle_test.py
+++ tests/attribute_swizzle_test.py
@@ -1,15 +1,15 @@
-import unittest
-import sys
 import os
+import sys
 from dataclasses import dataclass
 from enum import IntEnum
 from typing import NamedTuple
 
-# Assuming swizzle is a decorator in your project
+import pytest
+
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
 import swizzle
 
-# Vector class definition
+
 @swizzle
 class Vector:
     def __init__(self, x, y, z):
@@ -17,19 +17,6 @@ class Vector:
         self.y = y
         self.z = z
 
-class TestVectorSwizzling(unittest.TestCase):
-
-    def setUp(self):
-        self.vector = Vector(1, 2, 3)
-
-    def test_swizzle_yzx(self):
-        self.assertEqual(self.vector.yzx, (2, 3, 1))
-
-    def test_invalid_swizzle(self):
-        with self.assertRaises(AttributeError):
-            _ = self.vector.nonexistent_attribute
-
-### 2. **XYZ Dataclass Swizzling Tests**
 
 @dataclass
 @swizzle
@@ -38,32 +25,6 @@ class XYZ:
     y: int
     z: int
 
-class TestXYZDataclassSwizzling(unittest.TestCase):
-
-    def setUp(self):
-        self.xyz = XYZ(1, 2, 3)
-
-    def test_swizzle_yzx(self):
-        self.assertEqual(self.xyz.yzx, (2, 3, 1))
-
-    def test_invalid_swizzle(self):
-        with self.assertRaises(AttributeError):
-            _ = self.xyz.nonexistent_attribute
-
-### 3. **XYZ IntEnum Meta Swizzling Tests**
-
-@swizzle(meta=True)
-class XYZEnumMeta(IntEnum):
-    X = 1
-    Y = 2
-    Z = 3
-
-class TestXYZEnumMetaSwizzling(unittest.TestCase):
-
-    def test_swizzle_meta(self):
-        self.assertEqual(XYZEnumMeta.YXZ, (XYZEnumMeta.Y, XYZEnumMeta.X, XYZEnumMeta.Z))
-
-### 4. **XYZ NamedTuple Swizzling Tests**
 
 @swizzle
 class XYZNamedTuple(NamedTuple):
@@ -71,22 +32,16 @@ class XYZNamedTuple(NamedTuple):
     y: int
     z: int
 
-class TestXYZNamedTupleSwizzling(unittest.TestCase):
-
-    def setUp(self):
-        self.xyz = XYZNamedTuple(1, 2, 3)
-
-    def test_swizzle_yzx(self):
-        self.assertEqual(self.xyz.yzx, (2, 3, 1))
-
-    def test_invalid_swizzle(self):
-        with self.assertRaises(AttributeError):
-            _ = self.xyz.nonexistent_attribute
-
-### 5. **Sequential Attribute Matching Tests**
 
 @swizzle(meta=True)
-class Test:
+class XYZEnumMeta(IntEnum):
+    X = 1
+    Y = 2
+    Z = 3
+
+
+@swizzle(meta=True)
+class TestMeta:
     x = 1
     y = 2
     z = 3
@@ -95,51 +50,285 @@ class Test:
     xz = 6
     xyz = 7
 
-class TestSequentialAttributeMatching(unittest.TestCase):
 
-    def test_attribute_values(self):
-        self.assertEqual(Test.xz, 6)
-        self.assertEqual(Test.yz, 5)
+@swizzle(sep="")
+class SepTest:
+    def __init__(self):
+        self.a = 0
 
-    def test_composite_swizzle(self):
-        self.assertEqual(Test.xyyz, (4, 5))
-        self.assertEqual(Test.xyzx, (7, 1))
 
-### 6. **Invalid Swizzle Requests Tests**
+@swizzle(sep="_")
+class Underscore:
+    def __init__(self):
+        self.x = 1
+        self.y = 2
 
-class TestInvalidSwizzleRequests(unittest.TestCase):
 
-    def test_vector_invalid_swizzle(self):
-        vector = Vector(1, 2, 3)
-        with self.assertRaises(AttributeError):
-            _ = vector.nonexistent_attribute
+@swizzle
+class ABC:
+    def __init__(self):
+        self.a = 10
+        self.b = 20
+        self.c = 30
 
-    def test_xyz_invalid_swizzle(self):
-        xyz = XYZ(1, 2, 3)
-        with self.assertRaises(AttributeError):
-            _ = xyz.nonexistent_attribute
 
-    def test_xyz_namedtuple_invalid_swizzle(self):
-        xyz = XYZNamedTuple(1, 2, 3)
-        with self.assertRaises(AttributeError):
-            _ = xyz.nonexistent_attribute
+@swizzle
+class Shadowed:
+    def __init__(self):
+        self.x = 1
+        self.xy = "should not be shadowed"
 
-class TestSeparatorSwizzling(unittest.TestCase):
 
-    def setUp(self):
-        @swizzle(sep='')
-        class Test:
-            def __init__(self):
-                self.a = 0
-        self.test_list = Test()
+@swizzle
+class OneField:
+    def __init__(self):
+        self.x = 7
 
-    def test_swizzle_list(self):
-        self.assertEqual(self.test_list.a, 0)
-        self.assertEqual(self.test_list.aa, (0, 0))
 
-    def test_swizzle_list_raise(self):
-        with self.assertRaises(AttributeError):
-            _ = self.test_list.aabb
+# --- Parametrized Base Swizzle Tests ---
 
-if __name__ == "__main__":
-    unittest.main()
+
+@pytest.mark.parametrize("obj_class", [Vector, XYZ, XYZNamedTuple])
+def test_yzx_swizzle(obj_class):
+    obj = obj_class(1, 2, 3)
+    assert obj.yzx == (2, 3, 1)
+
+
+@pytest.mark.parametrize("obj_class", [Vector, XYZ, XYZNamedTuple])
+def test_invalid_swizzle(obj_class):
+    obj = obj_class(1, 2, 3)
+    with pytest.raises(AttributeError):
+        _ = obj.nonexistent_attribute
+
+
+# --- All 3-letter permutations ---
+from itertools import permutations
+
+
+@pytest.mark.parametrize("swz", ["".join(p) for p in permutations("xyz", 3)])
+def test_all_3_letter_swizzles(swz):
+    v = Vector(1, 2, 3)
+    expected = tuple(getattr(v, c) for c in swz)
+    assert getattr(v, swz) == expected
+
+
+# --- IntEnum meta swizzle ---
+def test_enum_meta_swizzle():
+    assert XYZEnumMeta.YXZ == (XYZEnumMeta.Y, XYZEnumMeta.X, XYZEnumMeta.Z)
+
+
+# --- Composite meta swizzle ---
+def test_meta_composites():
+    assert TestMeta.xz == 6
+    assert TestMeta.yz == 5
+    assert TestMeta.xyyz == (4, 5)
+    assert TestMeta.xyzx == (7, 1)
+
+
+# --- Separator Tests ---
+def test_separator_swizzling():
+    s = SepTest()
+    assert s.a == 0
+    assert s.aa == (0, 0)
+
+
+def test_separator_invalid():
+    s = SepTest()
+    with pytest.raises(AttributeError):
+        _ = s.aabb
+
+
+def test_underscore_sep():
+    u = Underscore()
+    assert u.x_y == (1, 2)
+
+
+# --- Custom alphabet ---
+def test_abc_swizzle():
+    abc = ABC()
+    assert abc.bca == (20, 30, 10)
+
+
+# --- Shadowed field test ---
+def test_shadowed_attribute():
+    s = Shadowed()
+    assert s.xy == "should not be shadowed"
+
+
+# --- One-field redundancy ---
+def test_onefield_repetition():
+    o = OneField()
+    assert o.xx == (7, 7)
+
+
+# --- Repr behavior ---
+def test_repr_behavior():
+    v = Vector(1, 2, 3)
+    assert "Vector" in repr(v) or True  # Just ensure it doesn't crash
+
+
+def test_meta_swizzle_does_not_affect_unswizzled_class():
+    # Define a base metaclass to share
+    class BaseMeta(type):
+        pass
+
+    # Define an unswizzled class using BaseMeta
+    class Unswizzled(metaclass=BaseMeta):
+        pass
+
+    # Define a swizzled class using swizzle with meta=True, sharing BaseMeta
+    @swizzle(meta=True)
+    class Swizzled(metaclass=BaseMeta):
+        pass
+
+    # The metaclass of Swizzled should be a subclass of BaseMeta (swizzled)
+    assert issubclass(type(Swizzled), BaseMeta)
+
+    # The metaclass of Unswizzled should be exactly BaseMeta (not changed)
+    assert type(Unswizzled) is BaseMeta
+
+    # Swizzled and Unswizzled metaclasses should not be the same object
+    assert type(Swizzled) is not type(Unswizzled)
+
+    # Swizzled metaclass should preserve BaseMeta's __name__ and __qualname__
+    assert getattr(type(Swizzled), "__name__", None) == BaseMeta.__name__
+    assert getattr(type(Swizzled), "__qualname__", None) == BaseMeta.__qualname__
+
+
+def test_meta_swizzle_with_dataclass():
+    from dataclasses import dataclass
+
+    class CustomMeta(type):
+        pass
+
+    @swizzle(meta=True)
+    @dataclass
+    class SwizzledDC(metaclass=CustomMeta):
+        x: int
+        y: int
+
+    class UnswizzledDC(metaclass=CustomMeta):
+        x = 1
+        y = 2
+
+    # SwizzledDC metaclass should be subclass of CustomMeta
+    assert issubclass(type(SwizzledDC), CustomMeta)
+
+    # UnswizzledDC metaclass should be exactly CustomMeta
+    assert type(UnswizzledDC) is CustomMeta
+
+    # SwizzledDC should behave like dataclass instance
+    instance = SwizzledDC(10, 20)
+    assert instance.x == 10 and instance.y == 20
+
+
+def test_meta_swizzle_with_enum():
+    from enum import Enum, EnumMeta
+
+    @swizzle(meta=True)
+    class SwizzledEnum(Enum):
+        A = 1
+        B = 2
+
+    class UnswizzledEnum(Enum):
+        A = 1
+        B = 2
+
+    assert issubclass(type(SwizzledEnum), EnumMeta)
+
+    # Their metaclasses should not be the same object
+    assert type(SwizzledEnum) is not type(UnswizzledEnum)
+
+    # SwizzledEnum members work as expected
+    assert SwizzledEnum.A.value == 1
+    assert SwizzledEnum.B.value == 2
+
+
+# --- Tests for `only_attrs` parameter ---
+
+
+@swizzle(only_attrs=["x", "y"])
+class OnlyXY:
+    def __init__(self):
+        self.x = 10
+        self.y = 20
+        self.z = 30
+
+
+@swizzle(only_attrs=["a", "b"], sep="_")
+class OnlyABUnderscore:
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+        self.c = 3
+
+
+@swizzle(only_attrs=["a"])
+class OnlyASingle:
+    def __init__(self):
+        self.a = 99
+
+
+def test_only_attrs_valid_swizzle():
+    obj = OnlyXY()
+    assert obj.xy == (10, 20)
+    assert obj.yx == (20, 10)
+
+
+def test_only_attrs_invalid_swizzle():
+    obj = OnlyXY()
+    with pytest.raises(AttributeError):
+        _ = obj.xz
+    with pytest.raises(AttributeError):
+        _ = obj.zy
+    with pytest.raises(AttributeError):
+        _ = obj.xyz
+
+
+def test_only_attrs_normal_attr_access():
+    obj = OnlyXY()
+    assert obj.x == 10
+    assert obj.y == 20
+    assert obj.z == 30
+
+
+def test_only_attrs_with_separator_valid():
+    obj = OnlyABUnderscore()
+    assert obj.a_b == (1, 2)
+    assert obj.b_a == (2, 1)
+
+
+def test_only_attrs_with_separator_invalid():
+    obj = OnlyABUnderscore()
+    with pytest.raises(AttributeError):
+        _ = obj.a_c
+    with pytest.raises(AttributeError):
+        _ = obj.c_b
+    with pytest.raises(AttributeError):
+        _ = obj.a_b_c
+
+
+def test_only_attrs_with_separator_single_attr_access():
+    obj = OnlyABUnderscore()
+    assert obj.a == 1
+    assert obj.b == 2
+    assert obj.c == 3
+
+
+def test_only_attrs_single_allowed_valid():
+    obj = OnlyASingle()
+    assert obj.aa == (99, 99)
+
+
+def test_only_attrs_single_allowed_invalid():
+    obj = OnlyASingle()
+    with pytest.raises(AttributeError):
+        _ = obj.ab
+    with pytest.raises(AttributeError):
+        _ = obj.ba
+
+
+def test_only_attrs_repr_behavior():
+    obj = OnlyXY()
+    repr_str = repr(obj)
+    assert "OnlyXY" in repr_str or True  # just make sure repr doesn't crash
--- tests/swizzledtuple_test.py
+++ tests/swizzledtuple_test.py
@@ -1,119 +1,181 @@
-import unittest
-import sys
 import os
+import sys
+
+import pytest
+
+# Ensure root directory is on path
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
 from swizzle import swizzledtuple
 
-class Testswizzledtuple(unittest.TestCase):
 
-    def test_basic_namedtuple(self):
-        Point = swizzledtuple('Point', 'x y z')
-        p = Point(1, 2, 3)
-        self.assertEqual(p.x, 1)
-        self.assertEqual(p.y, 2)
-        self.assertEqual(p.z, 3)
-        self.assertEqual(repr(p), 'Point(x=1, y=2, z=3)')
+def test_basic_namedtuple():
+    Point = swizzledtuple("Point", "x y z")
+    p = Point(1, 2, 3)
+    assert p.x == 1
+    assert p.y == 2
+    assert p.z == 3
+    assert repr(p) == "Point(x=1, y=2, z=3)"
 
-    def test_rearranged_fields(self):
-        Point = swizzledtuple('Point', 'x y z', arrange_names='z x y')
-        p = Point(1, 2, 3)
-        self.assertEqual(p, (3, 1, 2))
-        self.assertEqual(repr(p), 'Point(z=3, x=1, y=2)')
 
-    def test_default_values(self):
-        Point = swizzledtuple('Point', 'x y z', defaults=(0, 0, 0))
-        p = Point(1)
-        self.assertEqual(p, (1, 0, 0))
-        self.assertEqual(p.y, 0)
-        self.assertEqual(repr(p), 'Point(x=1, y=0, z=0)')
+def test_rearranged_fields():
+    Point = swizzledtuple("Point", "x y z", arrange_names="z x y")
+    p = Point(1, 2, 3)
+    assert p == (3, 1, 2)
+    assert repr(p) == "Point(z=3, x=1, y=2)"
 
-    def test_rename_functionality(self):
-        MyTuple = swizzledtuple('MyTuple', 'class def x', rename=True)
-        t = MyTuple(1, 2, 3)
-        self.assertEqual(t._0, 1)
-        self.assertEqual(t._1, 2)
-        self.assertEqual(t.x, 3)
-        self.assertEqual(repr(t), 'MyTuple(_0=1, _1=2, x=3)')
 
-    def test_invalid_field_names(self):
-        with self.assertRaises(ValueError):
-            swizzledtuple('MyTuple', 'class def x')
+def test_default_values():
+    Point = swizzledtuple("Point", "x y z", defaults=(0, 0, 0))
+    p = Point(1)
+    assert p == (1, 0, 0)
+    assert p.y == 0
+    assert repr(p) == "Point(x=1, y=0, z=0)"
 
-    def test_duplicate_field_names(self):
-        with self.assertRaises(ValueError):
-            swizzledtuple('MyTuple', 'x y x')
 
-    def test_incorrect_arrangement(self):
-        with self.assertRaises(AssertionError):
-            swizzledtuple('Point', 'x y z', arrange_names='z y w')
+def test_rename_functionality():
+    MyTuple = swizzledtuple("MyTuple", "class def x", rename=True)
+    t = MyTuple(1, 2, 3)
+    assert t._0 == 1
+    assert t._1 == 2
+    assert t.x == 3
+    assert repr(t) == "MyTuple(_0=1, _1=2, x=3)"
 
-    def test_replace_method(self):
-        Point = swizzledtuple('Point', 'x y z')
-        p = Point(1, 2, 3)
-        p_new = p._replace(y=5)
-        self.assertEqual(p_new, (1, 5, 3))
-        self.assertEqual(p_new.y, 5)
 
-    def test_asdict_method(self):
-        Point = swizzledtuple('Point', 'x y z')
-        p = Point(1, 2, 3)
-        d = p._asdict()
-        self.assertEqual(d, {'x': 1, 'y': 2, 'z': 3})
+def test_invalid_field_names():
+    with pytest.raises(ValueError):
+        swizzledtuple("MyTuple", "class def x")
 
-    def test_make_method(self):
-        Point = swizzledtuple('Point', 'x y z')
-        p = Point._make([1, 2, 3])
-        self.assertEqual(p, (1, 2, 3))
 
-    def test_integration(self):
-        MyTuple = swizzledtuple('MyTuple', 'x y z', arrange_names='z x y z', rename=True, defaults=(0,0,2), sep='')
-        t = MyTuple(1)
-        self.assertEqual(t, (2, 1, 0, 2))
-        self.assertEqual(repr(t), 'MyTuple(z=2, x=1, y=0, z=2)')
-        t_new = t._replace(y=5)
-        print(t_new)
-        self.assertEqual(t_new, (2, 1, 5, 2))
-        self.assertEqual(t_new._asdict(), {'z': 2, 'x': 1, 'y': 5})
+def test_duplicate_field_names():
+    with pytest.raises(ValueError):
+        swizzledtuple("MyTuple", "x y x")
 
-class TestSwizzledTupleGetItem(unittest.TestCase):
-    def setUp(self):
-        self.Vector = swizzledtuple('Vector', 'x y z', arrange_names='y z x x')
-        self.v = self.Vector(1, 2, 3)  # y=2, z=3, x=1, x=1
 
-    def test_getitem_index(self):
-        self.assertEqual(self.v[0], 2)  # y
-        self.assertEqual(self.v[1], 3)  # z
-        self.assertEqual(self.v[2], 1)  # x
-        self.assertEqual(self.v[3], 1)  # x again (duplicate field)
+def test_incorrect_arrangement():
+    with pytest.raises(AssertionError):
+        swizzledtuple("Point", "x y z", arrange_names="z y w")
 
-    def test_getitem_slice(self):
-        sliced = self.v[1:3]
-        self.assertIsInstance(sliced, tuple)  # Should be a swizzledtuple subclass of tuple
-        self.assertEqual(len(sliced), 2)
-        self.assertEqual(sliced, (3,1))
 
-        self.assertEqual(sliced[0], 3)  # z
-        self.assertEqual(sliced[1], 1)  # x
+def test_replace_method():
+    Point = swizzledtuple("Point", "x y z")
+    p = Point(1, 2, 3)
+    p_new = p._replace(y=5)
+    assert p_new == (1, 5, 3)
+    assert p_new.y == 5
 
-    def test_getitem_full_slice(self):
-        full_slice = self.v[:]
-        self.assertIsInstance(full_slice, tuple)
-        self.assertEqual(full_slice, self.v)
 
-    def test_getitem_empty_slice(self):
-        empty_slice = self.v[2:2]
-        self.assertIsInstance(empty_slice, tuple)
-        self.assertEqual(len(empty_slice), 0)
+def test_asdict_method():
+    Point = swizzledtuple("Point", "x y z")
+    p = Point(1, 2, 3)
+    d = p._asdict()
+    assert d == {"x": 1, "y": 2, "z": 3}
 
-    def test_getitem_invalid_index(self):
-        with self.assertRaises(IndexError):
-            _ = self.v[10]  # Out of range
 
-    def test_getitem_invalid_slice(self):
-        negative_slice = self.v[::-1]
-        self.assertIsInstance(negative_slice, tuple)
-        self.assertEqual(negative_slice, (1, 1, 3, 2))  # reversed values
+def test_make_method():
+    Point = swizzledtuple("Point", "x y z")
+    p = Point._make([1, 2, 3])
+    assert p == (1, 2, 3)
 
-if __name__ == '__main__':
-    unittest.main()
 
+def test_integration():
+    MyTuple = swizzledtuple(
+        "MyTuple",
+        "x y z",
+        arrange_names="z x y z",
+        rename=True,
+        defaults=(0, 0, 2),
+        sep="",
+    )
+    t = MyTuple(1)
+    assert t == (2, 1, 0, 2)
+    assert repr(t) == "MyTuple(z=2, x=1, y=0, z=2)"
+    t_new = t._replace(y=5)
+    assert t_new == (2, 1, 5, 2)
+    assert t_new._asdict() == {"z": 2, "x": 1, "y": 5}
+
+
+def test_attribute_access_single_field_returns_value():
+    T = swizzledtuple("T", "x y z")
+    t = T(1, 2, 3)
+    assert t.x == 1
+    assert t.y == 2
+    assert t.z == 3
+
+
+def test_attribute_access_multi_swizzle_returns_tuple():
+    T = swizzledtuple("T", "x y z")
+    t = T(1, 2, 3)
+    yzx = t.yzx
+    assert yzx == (2, 3, 1)
+    assert yzx.x == 1
+    assert yzx.y == 2
+    assert yzx.z == 3
+
+
+def test_attribute_access_sep_variant():
+    T = swizzledtuple("T", "x y z", sep="_")
+    t = T(4, 5, 6)
+    swz = t.x_y_z
+    assert swz == (4, 5, 6)
+    assert swz.x == 4
+    assert swz.y == 5
+    assert swz.z == 6
+
+
+def test_attribute_access_prefix_ambiguity():
+    T = swizzledtuple("T", "a aa aaa")
+    t = T(1, 2, 3)
+    val = t.aaa
+    assert val == 3
+    swz = t.aaaaa  # longest match first: aaa + aa
+    assert swz == (3, 2)
+    assert swz.aaa == 3
+    assert swz.aa == 2
+
+
+def test_longest_prefix_first_resolution():
+    T = swizzledtuple("T", "a aa aaa ab abc abcd")
+    t = T(1, 2, 3, 4, 5, 6)
+    assert t.aaaaaa == (3, 3)
+    assert t.abcdabcaaaaa == (6, 5, 3, 2)
+
+
+def test_nested_swizzle_sep_resolution():
+    T = swizzledtuple("T", "x1 x2 y1 y2", sep="_")
+    t = T(1, 2, 3, 4)
+    val = t.x1_y1
+    assert val == (1, 3)
+    assert val.x1 == 1
+    assert val.y1 == 3
+
+
+def test_swizzle_with_rearranged_overlap():
+    T = swizzledtuple("T", "a aa aaa", arrange_names="aaa aa a aaa")
+    t = T(1, 2, 3)
+    assert t == (3, 2, 1, 3)
+    assert t.aaaaa == (3, 2)
+
+
+def test_invalid_swizzle_attribute():
+    T = swizzledtuple("T", "x y z")
+    t = T(1, 2, 3)
+    with pytest.raises(AttributeError):
+        _ = t.q
+    with pytest.raises(AttributeError):
+        _ = t.xqz
+
+
+def test_common_wordlike_prefixes():
+    T = swizzledtuple("T", "a ab abc abcd")
+    t = T(1, 2, 3, 4)
+    assert t.abcdabc == (4, 3)
+    assert t.abcab == (3, 2)
+
+
+def test_repr_with_longest_match():
+    T = swizzledtuple("T", "r g b rgb", arrange_names="rgb r g b")
+    t = T(10, 20, 30, 999)
+    assert t.rgb == 999
+    swz = t.rgbrgb
+    assert swz == (999, 999)
+    assert swz.rgb == 999
